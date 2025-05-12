0fc5d1268f9fd329bd9c6794a28d22b67282e6ef
zaedian <55536305+zaedian@users.noreply.github.com>
2025-05-12 12:02:52 +0200

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -996,7 +996,7 @@ const checkPortalEntry = (portal) => {
     const cameraHeightOffset = player.userData.height * 0.4; // Adjust to eye level
     camera.position.set(
         playerPosition.x,
-        playerPosition.y - cameraHeightOffset / 1.5,
+        playerPosition.y - cameraHeightOffset / 2,
         playerPosition.z
     );
 
